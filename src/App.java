568948247cb0d1540779614251674fb4168fa096
dulytime <dulyapap2000@gmail.com>
2023-05-10 15:55:17 +0700

    Test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Capco");
+        float target;
+        Scanner a = new Scanner(System.in);
+        System.out.print("Enter number of Array : ");
+        int n = a.nextInt();
+        double []nums =new double[n+1];
+        for (int i=0;i<n;i++){
+            System.out.print("Enter number "+i+" : ");
+            nums[i]=a.nextDouble();
+        }
+        System.out.print("Enter target : ");
+        target = a.nextFloat();
+        System.out.println("Answer = "+indicearray(nums, target));
+    }
+    public static int indicearray(double[]num,float target){
+        int y=0,answer = 0;
+        for (int x=0;x<num.length-1;x++){
+            y=x+1;
+            if (num[x]+num[y]==target){
+                answer = x+y;
+            }
+        }
+        return answer;
     }
 }
